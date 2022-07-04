a6925ecb0820dcf29e1d2b22760899354ddaafc5
Simca <mmosimca@gmail.com>
2022-07-04 12:23:17 -0400

    Added QuestObjective struct output to a separate CSV.

---
+++ WDBReader/WDBSchema/QuestObjectiveMap.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration;
+
+namespace WDBReader.WDBSchema
+{
+    sealed class QuestObjectiveMap : ClassMap<QuestObjective>
+    {
+        public QuestObjectiveMap()
+        {
+            Map(m => m.QuestID);
+            Map(m => m.ID);
+            Map(m => m.Type);
+            Map(m => m.StorageIndex);
+            Map(m => m.AssetID);
+            Map(m => m.Amount);
+            Map(m => m.Flags).Index(6, 7);
+            Map(m => m.PercentAmount);
+            Map(m => m.NumVisualEffects);
+            Map(m => m.CombinedVisualEffects);
+            Map(m => m.Description);
+        }
+    }
+}
